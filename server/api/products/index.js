eecb3796730ceeec9ec7ad8534b3fe87ac9a5bdb
RABUNTHOEUN <rabunthoeun7777@gmail.com>
2025-07-23 17:12:14 +0700

    update

--- server/api/products/index.js
+++ server/api/products/index.js
@@ -2,7 +2,9 @@
 // server/api/products/index.ts
 let products = [
   { id: 1, name: 'iPhone 15', price: 999 },
-  { id: 2, name: 'MacBook Pro', price: 1999 },
+  { id: 2, name: 'MacBook Pro 2020', price: 1999 },
+  { id: 3, name: 'MacBook Pro 2024', price: 2999 },
+  { id: 4, name: 'MacBook Pro 2025', price: 3999 },
 ];
 
 export default defineEventHandler((event) => {
